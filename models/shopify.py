d521650cd0508bed0d70daf1b2f0980d4fe5bb2c
Carlos Fletes <carlos.fletes@marvelsa.com>
2020-10-22 09:38:11 -0500

    1.0.1

--- models/shopify.py
+++ models/shopify.py
@@ -2,20 +2,21 @@
 from odoo import fields, models
 
 
-class ProductTemplate(models.Model):
+class ProductTemplateShopify(models.Model):
     _inherit = "product.template"
 
     x_studio_image_shopify = fields.Binary(string='Imagen Sopify',
                                            store=True, attachment=True)
 
     x_studio_website_shopify = fields.Boolean(default=False,
-                                              string='Website Shopify', )
+                                              string='Website Shopify')
 
 
-class SaleOrder(models.Model):
+class SaleOrderShopify(models.Model):
     _inherit = "sale.order"
 
     x_studio_metodo_de_pago = fields.Char(string='Metodo de Pago',)
-    x_studio_metodo_de_envio_shopify = fields.Char(string='Metodo de Envio Shopify',)
+    x_studio_metodo_de_envio_shopify = fields.Char(
+        string='Metodo de Envio Shopify')
     x_studio_pago_con_gift_cards = fields.Boolean(default=False,
-                                                  string='Pago con Gift Cards', )
+                                                  string='Pago con Gift Cards')
