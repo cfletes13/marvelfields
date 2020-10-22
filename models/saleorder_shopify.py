93198cf4a4f61e0674489e84bb348177c2b52b66
Carlos Fletes <carlos.fletes@marvelsa.com>
2020-10-22 12:04:01 -0500

    1.0.3

--- models/saleorder_shopify.py
+++ models/saleorder_shopify.py
@@ -2,10 +2,6 @@
 from odoo import fields, models
 
 
-class SaleOrderShopify(models.Model):
+class ShopifySaleOrder(models.Model):
     _inherit = 'sale.order'
-
-    x_studio_metodo_de_pago = fields.Char(string='Metodo de Pago',)
-    x_studio_metodo_de_envio_shopify = fields.Char(string='Metodo de Envio Shopify')
-    x_studio_pago_con_gift_cards = fields.Boolean(default=False,
-                                                  string='Pago con Gift Cards')
+    x_studio_metodo_de_pago = fields.Char(string="Metodo de Pago")
