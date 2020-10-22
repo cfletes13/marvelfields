10433bb1cf97dccedb221c4a57d6870604eb2966
Carlos Fletes <carlos.fletes@marvelsa.com>
2020-10-22 11:29:13 -0500

    1.0.2

--- models/shopify.py
+++ models/shopify.py
@@ -5,8 +5,8 @@ from odoo import fields, models
 class ProductTemplateShopify(models.Model):
     _inherit = 'product.template'
 
-    image_shopify = fields.Binary(string='Imagen Sopify',
-                                  store=True, attachment=True)
+    x_studio_image_shopify = fields.Binary(string='Imagen Sopify',
+                                           store=True, attachment=True)
 
-    website_shopify = fields.Boolean(default=False,
-                                     string='Website Shopify')
+    x_studio_website_shopify = fields.Boolean(default=False,
+                                              string='Website Shopify')
